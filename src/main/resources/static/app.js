348395d87b553a89f7e38109bb0a437da9e71c3c
projsh <projsh@users.noreply.github.com>
2020-07-24 13:40:35 +1000

    not much changed but need to move to desktop

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -56,25 +56,27 @@ let mediaControls = new Vue({
 
 let mainSongList = Vue.extend({
     template:
-    `<div class="list-container">
-        <div class="list-section">
-        <i class="material-icons-outlined play-pause" style="visibility: hidden;">play_arrow</i>
-        <div class="artist-title-album section">
-            <p class="list-title">Title</p>
-            <p class="list-artist">Artist</p>
-            <p class="list-album">Album</p>
-            <p class="list-duration">Duration</p>
-        </div>
-        </div>
-        <li v-for="item in songs" v-bind:song="item" v-on:mouseover="hover($event)" v-on:mouseleave="leave($event)" v-on:dblclick="play(item)" v-on:click="play(item, true)" :skey="item.id" :key="item.id" class="results-link">
-            <i class="material-icons-outlined play-pause" style="opacity: 0;">play_arrow</i>
-            <div class="artist-title-album">
-                <p class="list-title">{{ item.title }}</p>
-                <p class="list-artist"><span>{{item.artist}}</span></p>
-                <p class="list-album"><span>{{item.album}}</span></p>
-                <p class="list-duration"><span>{{item.dur}}</span></p>
+    `<div class="tab-container">
+        <div class="list-container">
+            <div class="list-section">
+            <i class="material-icons-outlined play-pause" style="visibility: hidden;">play_arrow</i>
+            <div class="artist-title-album section">
+                <p class="list-title">Title</p>
+                <p class="list-artist">Artist</p>
+                <p class="list-album">Album</p>
+                <p class="list-duration">Duration</p>
             </div>
-        </li>
+            </div>
+            <li v-for="item in songs" v-bind:song="item" v-on:mouseover="hover($event)" v-on:mouseleave="leave($event)" v-on:dblclick="play(item)" v-on:click="play(item, true)" :skey="item.id" :key="item.id" class="results-link">
+                <i class="material-icons-outlined play-pause" style="opacity: 0;">play_arrow</i>
+                <div class="artist-title-album">
+                    <p class="list-title">{{ item.title }}</p>
+                    <p class="list-artist"><span>{{item.artist}}</span></p>
+                    <p class="list-album"><span>{{item.album}}</span></p>
+                    <p class="list-duration"><span>{{item.dur}}</span></p>
+                </div>
+            </li>
+        </div>
     </div>`,
     methods: {
         play(song, isTouch) {
@@ -195,17 +197,20 @@ let mainSongListComp = new mainSongList({
     }
 });
 
-mainSongListComp.$mount('#mount-point');
+mainSongListComp.$mount('.tab-container');
+
+let allSongs = [];
 
 fetch(`http://${hostnamePort}/api/getAllSongs`).then(resp => {
     resp.json().then(songs => {
         songs.forEach((f, i) => {
             if (f.title == null || f.artist == null || f.album == null || f.title == "" || f.artist == "" || f.album == "") {
-                mainSongListComp.songs.push({id: f.id, title: f.fileName, artist: "Unknown Artist", album: "Unknown Album", fileName: f.fileName, dur: f.duration});
+                allSongs.push({id: f.id, title: f.fileName, artist: "Unknown Artist", album: "Unknown Album", fileName: f.fileName, dur: f.duration});
             } else {
-                mainSongListComp.songs.push({id: f.id, title: f.title, artist: f.artist, album: f.album, fileName: f.fileName, dur: f.duration});
+                allSongs.push({id: f.id, title: f.title, artist: f.artist, album: f.album, fileName: f.fileName, dur: f.duration});
             }
-            sortArray(mainSongListComp.songs, 'artist')
+            sortArray(allSongs, 'artist');
+            mainSongListComp.songs = allSongs;
         })
     })
 });
@@ -372,18 +377,20 @@ try {
 
 let currentActiveTab = 'allButton'
 
+let mountedTab = null;
+
 let sidebar = new Vue({
     el: '.temp-sidebar',
     template: 
     `<div class="sidebar-wrap">
         <div class="active-indicator" style="top: 85px"></div>
         <div v-for="item in sidebarItems" v-bind:sideitem="item" v-bind:id="item.id">
-            <div v-on:load="active($event)" :style="active" v-on:click="click($event)" v-if="item.type == 'item'" class="side-item side-click">
+            <div v-on:load="active($event)" :style="active" v-on:click="click($event, item)" v-if="item.type == 'item'" class="side-item side-click">
                 <i class="material-icons-outlined">{{ item.icon }}</i>
                 <span>{{ item.label }}</span>
             </div>
             <div v-else-if="item.type == 'label'" class="side-label">{{item.label}}</div>
-            <div v-else class="side-list-item side-click" v-on:click="click($event)" v-for="listItem in item.items" v-bind:listItems="listItem" v-bind:id="listItem.id">
+            <div v-else class="side-list-item side-click" v-on:click="click($event, listItem)" v-for="listItem in item.items" v-bind:listItems="listItem" v-bind:id="listItem.id">
                 <div>{{listItem.label}}</div>
             </div>
         </div>
@@ -391,32 +398,68 @@ let sidebar = new Vue({
     data() {
         return {
             sidebarItems: [
-                {id: 'homeButton', type: 'item', label: 'Home', icon: 'home'},
-                {id: 'settingsButton', type: 'item', label: 'Settings', icon: 'settings'},
+                {id: 'homeTab', type: 'item', label: 'Home', icon: 'home'},
+                {id: 'settingsTab', type: 'item', label: 'Settings', icon: 'settings'},
                 {id: 'libraryLabel', type: 'label', label: 'Library'},
-                {id: 'likedButton', type: 'item', label: 'Liked Songs', icon: 'favorite_border'},
-                {id: 'allButton', type: 'item', label: 'All Songs', icon: 'music_note'},
-                {id: 'artistsButton', type: 'item', label: 'Artists', icon: 'person'},
-                {id: 'albumsButton', type: 'item', label: 'Albums', icon: 'album'},
+                {id: 'likedTab', type: 'item', label: 'Liked Songs', icon: 'favorite_border'},
+                {id: 'allTab', type: 'item', label: 'All Songs', icon: 'music_note'},
+                {id: 'artistsTab', type: 'item', label: 'Artists', icon: 'person'},
+                {id: 'albumsTab', type: 'item', label: 'Albums', icon: 'album'},
                 {id: 'playlistLabel', type: 'label', label: 'Playlists'},
                 {id: 'playlistsList', type: 'list', items: [{id: 'test', label: 'test label'}]}
             ]
         }
     },
     methods: {
-        click(evt) {
+        click(evt, item) {
             console.log(evt);
             console.log(evt.target.getBoundingClientRect());
             document.querySelectorAll('.side-click').forEach(f => {
                 f.classList.remove('active');
-            })
+            });
             evt.target.classList.add('active');
             let indicator = document.querySelector('.active-indicator');
             indicator.style.height = evt.target.getBoundingClientRect().height + 'px';
             indicator.style.top = evt.target.getBoundingClientRect().top + 'px';
+            tabName.title = item.label;
+            if (mountedTab != null) {
+                mountedTab.$destroy;
+                document.querySelector('.tab-container').innerHTML = '';
+            }
+            switch(item.id) {
+                case "allTab":
+                    let mainSongListComp = new mainSongList({
+                        data: {
+                            songs: []
+                        }
+                    });
+                    mainSongListComp.songs = allSongs;
+                    mainSongListComp.$mount('.tab-container');
+                    mountedTab = mainSongListComp;
+                    break;
+                case "homeTab":
+                    let home = new homeTab;
+                    home.$mount('.tab-container');
+                    mountedTab = homeTab;
+            }
         },
         active(evt) {
 
         }
     }
+});
+
+let tabName = new Vue({
+    el: '.top-title',
+    data() {
+        return {
+            title: 'Home'
+        }
+    }
+})
+
+//home tab
+let homeTab = Vue.extend({
+    template: 
+    `<div class="tab-container"><p>lol</p></div>`
 });
